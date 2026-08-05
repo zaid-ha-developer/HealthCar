5725ea5f394dca11a9ce3d340fc97d0aed3ecc2b
zaid-hatahet-dev <zaid.hatahet.2023@gmai.com>
2026-08-05 10:17:43 +0300

    Add framer-motion for page transitions

--- healthcare/src/Component/Layout/Layout.js
+++ healthcare/src/Component/Layout/Layout.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react';
 import Navbar from '../Nav/Navbar';
-import { Outlet } from 'react-router-dom';
-import Footer from '../Footer/Footer';   
+import { Outlet, useLocation } from 'react-router-dom'; 
+import Footer from '../Footer/Footer';
+import { AnimatePresence, motion } from 'framer-motion'; 
 
 const Layout = () => {
+  const location = useLocation(); 
+
   return (
     <Fragment>
       <Navbar />
       <main>
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}  
+            initial={{ opacity: 0, y: 20 }}  
+            animate={{ opacity: 1, y: 0 }}   
+            exit={{ opacity: 0, y: -20 }}    
+            transition={{ duration: 0.3 }}   
+          >
+            <Outlet /> 
+          </motion.div>
+        </AnimatePresence>
       </main>
       <Footer />
     </Fragment>
